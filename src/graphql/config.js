6c6b9f0512426490d0ab1a7af3eaeaa7405ac95d
Dan <dan.bayford@gmail.com>
2021-08-24 21:37:12 -0400

    env variables

--- src/graphql/config.js
+++ src/graphql/config.js
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
     link: new HttpLink({
-      uri: 'https://dba-dev-jobs.hasura.app/v1/graphql',
+      uri: process.env.GRAPHQL_URL,
       headers: {
-        'x-hasura-admin-secret' : 'wEqjLTN3F95fAvyvN3V9lxGKhL2e8Xn2mF0QB3HB8gfbs1aMq84FNOF28aE8nlop'
+        'x-hasura-admin-secret' : process.env.GRAPHQL_SECRET
       }
     }),
       cache: new InMemoryCache()
