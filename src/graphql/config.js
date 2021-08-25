162780bfb8fc417653cb7792abf58fa72afd3809
Dan <dan.bayford@gmail.com>
2021-08-24 21:42:08 -0400

    env var

--- src/graphql/config.js
+++ src/graphql/config.js
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
     link: new HttpLink({
-      uri: process.env.GRAPHQL_URL,
+      uri: process.env.REACT_APP_GRAPHQL_URL,
       headers: {
-        'x-hasura-admin-secret' : process.env.GRAPHQL_SECRET
+        'x-hasura-admin-secret' : process.env.REACT_APP_GRAPHQL_SECRET
       }
     }),
       cache: new InMemoryCache()
